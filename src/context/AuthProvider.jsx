d14162914d870cde85d8333a22acb3bbd71e0d5a
IvanPz01 <ivanciito_999@hotmail.com>
2024-06-12 11:19:48 -0300

    cambios al context y logica

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer, useEffect } from "react";
+import { createContext, useReducer, useEffect, useState } from "react";
 import { AuthReducer } from "../context/authReducer";
 import { types } from "../types/types";
 
@@ -9,18 +9,42 @@ export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const token = localStorage.getItem('token') || null;
+    const [user,setUser] = useState({});
 
-    useEffect(() => {
-        if (token !== null) {
-            dispatch({
-                type: types.LOGIN,
-                payload: {
-                    logged: true,
-                    token
-                }
-            });
-        }
-    },[token]);
+    useEffect(()=>{
+
+        if(state.logged){
+            fetch('http://127.0.0.1:8000/auth/validate/token',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${state.token.token}`
+                },
+                
+        }).then(response => {
+            response.json()
+            if(response.status === 401){
+                dispatch({
+                    type: types.LOGOUT
+                })
+                localStorage.removeItem('token')
+            }
+            if(response.status === 200 && token !== null){
+                dispatch({
+                    type: types.LOGIN,
+                    payload: {
+                        logged: true,
+                        token
+                    }
+                });
+            }
+        })
+        .then(data => {
+            /* setUser(data) */
+            console.log(data)
+        })
+    }
+})
 
     const inicialState = {
         logged: false,
