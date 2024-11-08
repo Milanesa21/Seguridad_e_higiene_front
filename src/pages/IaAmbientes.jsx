39407fcd08230931bcfbcbd9ad9ead4511e3750a
Tonx117 <Tomasvaldez02@gmail.com>
2024-11-08 15:43:43 -0300

    LandingPage

--- src/pages/IaAmbientes.jsx
+++ src/pages/IaAmbientes.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Navbar } from '../components/Navbar';
 import { Footer } from '../components/Footer';
 import { EmergencyModal } from '../components/EmergencyModal';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,45 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Incluye archivos en `src`
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  // tailwind.config.js
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1F2937", // Gris oscuro
+        secondary: "#4B5563", // Gris intermedio
+        accent: "#F59E0B", // Naranja brillante
+        background: "#111827", // Gris casi negro
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"], // Fuente para una apariencia moderna
+        title: ["Roboto", "sans-serif"], // Fuente para títulos
+      },
     },
-    plugins: [
-      require('daisyui'),
-    ],
-  }
-  
+  },
+  // tailwind.config.js
+  theme: {
+    extend: {
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        slideInUp: {
+          "0%": { transform: "translateY(100%)" },
+          "100%": { transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 2s ease-in-out",
+        slideInUp: "slideInUp 1s ease-out",
+      },
+    },
+  },
+};
